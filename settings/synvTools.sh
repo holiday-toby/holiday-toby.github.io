d06e0584327b6ea1960ee2846db2f96d806035d5
ldh <lingduanhao@gamil.com>
2018-10-16 17:38:13 +0800

    setting 进行平台区分

--- settings/synvTools.sh
+++
@@ -1,5 +0,0 @@
-PREFIX="/Users/ldh/Learn/Vcps/"
-cp ~/.vimrc ./
-cp ~/.zshrc ./
-cp ~/.gitconfig ./
-cp /usr/local/git/etc/gitconfig ./
---
+++ settings/termux/synvTools.sh
@@ -0,0 +1,5 @@
+PREFIX="/Users/ldh/Learn/Vcps/"
+cp -iv ~/.vimrc ./vimrc
+cp -v ~/.zshrc ./zshrc
+cp -v ~/.gitconfig ./gitconfig
+cp -v /usr/local/git/etc/gitconfig ./usr-local-git-etc-gitconfig
